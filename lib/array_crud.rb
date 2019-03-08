c47a6d9a530e000170769a9f1e93fc06675901ba
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-08 06:44:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array ["Hi", "Bye"]
-  array.unshift( )
+  array.unshift("Greetings")
 end
 
 def remove_element_from_end_of_array(array)
