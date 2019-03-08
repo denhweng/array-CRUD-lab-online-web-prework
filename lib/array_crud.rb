1e6ebeead69199a1b41d6e3d913729c4f4e8e610
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-08 06:43:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Hi", "Bye"]
+  element = ["Greetings"]
+  array 
 end
 
 def add_element_to_start_of_array(array, element)
